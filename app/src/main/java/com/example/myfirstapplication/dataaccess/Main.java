253ab350af5239b378fd32842b6cc4e0f7f76591
Derron Li <104483680+derronli@users.noreply.github.com>
2024-04-03 01:53:31 -0400

    Update Main.java

--- app/src/main/java/com/example/myfirstapplication/dataaccess/Main.java
+++ app/src/main/java/com/example/myfirstapplication/dataaccess/Main.java
@@ -1,6 +1,7 @@
 package com.example.myfirstapplication.dataaccess;
 
 public class Main {
+    // THIS CLASS IS JUST FOR TESTING THE DATABASE FUNCTIONS
     public static void main(String[] args) {
         UserTableDAO userTableAccess = new UserTableDAO();
         System.out.println(userTableAccess.validateLogin("bob14", "bob123"));
